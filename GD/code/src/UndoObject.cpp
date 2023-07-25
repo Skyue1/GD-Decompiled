69b9d5bc7c26b8e4fac74cdb30510a68ff3fd984
skyue <104967297+Skyue1@users.noreply.github.com>
2023-07-25 14:44:04 +0900

    Update UndoObject.cpp

--- GD/code/src/UndoObject.cpp
+++ GD/code/src/UndoObject.cpp
@@ -1,1 +1,35 @@
+#include "../headers/includes.h"
 
+int UndoObject::initWithTransformObjects(cocos2d::CCArray* transformObjects)
+{
+  unsigned int index = 0;
+
+  auto newArray = cocos2d::CCArray::create();
+
+  while (index < transformObjects->count())
+  {
+    GameObjectCopy* gameObjectCopy = (GameObjectCopy*)transformObjects->objectAtIndex(index++);
+    cocos2d::CCObject* gameObject = (cocos2d::CCObject*)GameObjectCopy::create(gameObjectCopy);
+    newArray->addObject(gameObject);
+  }
+
+  newArray->retain();
+  return 1;
+}
+
+int UndoObject::createWithTransformObjects()
+{
+  int* newObj = new int[12]();
+
+	if (initWithTransformObjects(reinterpret_cast<cocos2d::CCArray*>(newObj)))
+	{
+		newObj = reinterpret_cast<int*>(CCObject::autorelease(reinterpret_cast<CCObject*>(newObj)));
+	}
+	else
+	{
+		delete[] newObj;
+		newObj = nullptr;
+	}
+
+	return newObj;
+}
